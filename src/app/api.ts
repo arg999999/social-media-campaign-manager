27a841ad7abac4d0010fcf1de91820a740127b8c
arg999999 <dev1.arg999@gmail.com>
2026-02-15 22:50:09 +0530

    repo updated

--- src/app/api.ts
+++ src/app/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    'https://campaignpulse-backend.onrender.com/api',
+    'https://social-media-campaign-manager-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
